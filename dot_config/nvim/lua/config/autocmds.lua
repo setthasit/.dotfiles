848f83205e3575551cd5e04af0156029fe727bae
setthasit <setthasit.th@gmail.com>
2026-09-20 03:03:57 -0700

    [AI] add jetbrains islands layout

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "VimEnter", "WinNew", "BufWinEnter" }, {
+  group = vim.api.nvim_create_augroup("islands", { clear = true }),
+  callback = function()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if vim.wo[win].winhighlight == "" then
+        vim.wo[win].winhighlight = "Normal:Island,NormalNC:Island"
+      end
+    end
+  end,
+})
